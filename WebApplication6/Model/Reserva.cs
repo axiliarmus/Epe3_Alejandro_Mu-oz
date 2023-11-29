5810589e76996b62ae33ff34cf23d047a3c80d85
axiliarmus <148289385+axiliarmus@users.noreply.github.com>
2023-11-29 01:16:48 -0300

    Update Reserva.cs

--- WebApplication6/Model/Reserva.cs
+++ WebApplication6/Model/Reserva.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApplication6.Model
+namespace WebApplication6.Model
 {
     public class Reserva
     {
+        public int id { get; set; }
+        public string? Especialidad { get; set; }
+        public DateTime? DiaReserva { get; set; }
+
+        public int? Paciente_idPaciente { get; set; }
+
+
     }
 }
